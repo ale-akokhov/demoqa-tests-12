e3d5881cdab0887de3ad003f38767678f4907fe2
Alexandr Akokhov <aleakokhov75@gmail.com>
2026-01-04 18:25:46 +0300

    improved build.gradle added SimpleTest.java created 2 tests with annotations

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,43 @@
+package guru.qa;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+
+    @BeforeAll
+    static void initDB() {
+        System.out.println("### @BeforeAll");
+    }
+
+    @BeforeEach
+    void openYaPage() {
+        System.out.println("###      @BeforeEach");
+        //Selenide.open("https://ya.ru");
+    }
+
+    @AfterEach
+    void closeYaPage() {
+        System.out.println("###      @AfterEach");
+        //WebDriverRunner.closeWindow();
+    }
+
+    @AfterAll
+    static void cleanDB() {
+        System.out.println("### @AfterAll");
+    }
+
+    @Test
+    void assertTest0() {
+        System.out.println("###           @Test 0");
+        Assertions.assertTrue(2 > 1);
+    }
+
+    @Test
+    void assertTest1() {
+        System.out.println("###           @Test 1");
+        Assertions.assertTrue(3 > 1);
+    }
+
+}
