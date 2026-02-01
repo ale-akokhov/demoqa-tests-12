57b94ef7c0df5fe1b4d37b330a6db8f967804782
Alexandr Akokhov <aleakokhov75@gmail.com>
2026-02-01 17:18:06 +0300

    PageObject added

--- src/test/java/guru/qa/pages/RegistrationFormPage.java
+++ src/test/java/guru/qa/pages/RegistrationFormPage.java
@@ -1,17 +1,25 @@
 package guru.qa.pages;
 
+import guru.qa.pages.components.CalenderComponent;
+
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormPage {
 
+    CalenderComponent calenderComponent = new CalenderComponent();
 
+
+    File file = new File("src/test/resources/picture1.jpg");
 
     public RegistrationFormPage openPage() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
         return this;
     }
 
@@ -35,5 +43,53 @@ public class RegistrationFormPage {
         return this;
     }
 
+    public RegistrationFormPage setMobileNumber(String value) {
+        $("#userNumber").setValue(value);
+        return this;
+    }
+
+    public RegistrationFormPage setDateOfBirth(String month, String year, String day) {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0" + day).click();
+        return this;
+    }
+
+    public RegistrationFormPage setSubjects(String value1, String value2) {
+        $("#subjectsInput").setValue(value1);
+        $("#subjectsInput").pressEnter();
+        $("#subjectsInput").setValue(value2);
+        $("#subjectsInput").pressEnter();
+        return this;
+    }
+    public RegistrationFormPage setHobbies(String value3, String value4) {
+        $("#hobbiesWrapper").$(byText(value3)).click();
+        $("#hobbiesWrapper").$(byText(value4)).click();
+        return this;
+    }
+
+    public RegistrationFormPage uploadPicture() {
+        $("#uploadPicture").uploadFile(file);
+        return this;
+    }
+
+    public RegistrationFormPage setCurrentAddress(String value) {
+        $("#currentAddress").setValue(value);
+        return this;
+    }
+
+    public RegistrationFormPage setStateAndCity(String value5, String value6) {
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(value5)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(value6)).click();
+        return this;
+    }
+
+    public RegistrationFormPage submit() {
+        $("#submit").click();
+        return this;
+    }
 
 }
